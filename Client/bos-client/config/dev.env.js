cd0620c91e344751fafdf44f3191eca26a76ec56
Muhammet Emin Aslangil <72221419+TR-Emin@users.noreply.github.com>
2023-03-20 16:40:54 +0300

    was added config/env files

---
+++ Client/bos-client/config/dev.env.js
@@ -0,0 +1,11 @@
+module.exports = {
+    DEBUG_MODE: true,
+    ENV: '"development"',
+    NODE_ENV: '"development"',
+    APP_ENV: '"development"',
+  
+    IDENTITY_SERVER_URL: '""',
+    ADMIN_API_URL: '"https://localhost:7041"'
+  };
+  
+  
---
+++ Client/bos-client/config/test.env.js
@@ -0,0 +1,11 @@
+module.exports = {
+    DEBUG_MODE: false,
+    ENV: '"production"',
+    NODE_ENV: '"production"',
+    APP_ENV: '"test"',
+  
+    IDENTITY_SERVER_URL: '""',
+    ADMIN_API_URL: '""'
+  
+  };
+  
